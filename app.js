879a1235165c03a95d21e98a2ca83c89ebbf8edf
Santa <has.hamza247@gmail.com>
2017-11-19 07:01:03 -0600

    created an idea schema

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
 const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 const port = 5000;
 
+//Map Global promise- get rid of warning
+mongoose.Promise = global.Promise
+//Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev', {
+    useMongoClient: true
+  })
+  .then(() => console.log('mongodb connected'))
+  .catch(err => console.log('error'))
+
+//Load Idea model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
+
 //handlebars middleware
-app.engine('handlebars',exphbs({
-  defaultLayout:'main'
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
 }));
-app.set('view engine','handlebars');
+app.set('view engine', 'handlebars');
 
 //index route
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
   const title = 'welcome'
-  res.render('index',{title: title})
+  res.render('index', {
+    title: title
+  })
 })
 
 //About route
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
   res.render('about')
 })
 
@@ -26,4 +43,4 @@ app.get('/about',(req,res)=>{
 
 
 
-app.listen(port,()=> console.log(`app is running on port ${port}`))
+app.listen(port, () => console.log(`app is running on port ${port}`))
